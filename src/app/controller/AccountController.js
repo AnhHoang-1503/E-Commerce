e3bc13448e22c374391475a19f65fe58a5d3c2ad
Vũ Trí Anh Hoàng <vutrianhhoang@gmail.com>
2023-05-09 17:53:28 +0700

    login && register

--- src/app/controller/AccountController.js
+++ src/app/controller/AccountController.js
@@ -1,23 +1,30 @@
 import Login from "../models/loginModel.js";
+import hasher from "../authentication/hasher.js";
 
-class AccountController {
-    // GET account/login
-    login(req, res, next) {
-        res.render("login");
-    }
-    // POST account/login
-    loginPost(req, res, next) {
-        res.redirect("/");
-    }
-    // GET account/register
-    register(req, res, next) {
-        res.render("register");
-    }
-    // POST account/register
-    registerPost(req, res, next) {
-        res.json(req.body);
-        res.redirect("/");
-    }
-}
+// GET account/login
+const login = function (req, res, next) {
+    res.render("login");
+};
+// POST account/login
+const loginPost = function (req, res, next) {
+    res.redirect("/");
+};
+// GET account/register
+const register = function (req, res, next) {
+    res.render("register");
+};
+// POST account/register
+const registerPost = function (req, res, next) {
+    const data = req.body;
+    hasher(data.password)
+        .then((output) => {
+            res.json({
+                email: data.email,
+                salt: output.salt,
+                hash: output.hash,
+            });
+        })
+        .catch(next);
+};
 
-export default new AccountController();
+export default { login, loginPost, register, registerPost };
